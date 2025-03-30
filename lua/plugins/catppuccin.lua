dcb6fc6e7a9940d14e45bc7d8c405ae49016b0d1
Fabian Fasse <fabianfasse@icloud.com>
2025-03-30 01:37:18 +0100

    update theme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        background = {
+          light = "latte",
+          dark = "mocha"
+        },
+        integrations = {
+          treesitter = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+          },
+          telescope = true,
+          cmp = true
+        }
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end
+  }
+}
