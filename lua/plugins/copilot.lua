9a7a32b90a2ae5f3533f8860a905b830857de2ca
Fabian Fasse <fabianfasse@icloud.com>
2025-03-30 15:52:37 +0200

    some updates

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    dependencies = { 'windwp/nvim-autopairs' },
+    event = "InsertEnter",
+    config = function()
+      local copilot = require 'copilot'
+      local suggestion = require 'copilot.suggestion'
+      local autopairs = require 'nvim-autopairs'
+
+      copilot.setup {
+        suggestion = {
+          auto_trigger = true,
+        },
+        filetypes = {
+          ['*'] = true,
+        },
+      }
+
+      vim.keymap.set('i', '<C-u>', function()
+        autopairs.disable()
+        suggestion.accept()
+        autopairs.enable()
+      end, { desc = 'Accept Copilot suggestion' })
+    end,
+  },
+}
