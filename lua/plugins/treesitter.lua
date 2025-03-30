f0f2824945de578e3fd3f3135cd989ab32619d58
Fabian Fasse <fabianfasse@icloud.com>
2025-03-30 01:36:34 +0100

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "javascript", "go", "typescript", "rust", "html", "angular", "css", "scss", "html", "tsx" },
+      ensure_installed = { "lua", "javascript", "go", "typescript", "rust", "html", "angular", "css", "scss", "html", "tsx", "svelte" },
       highlight = { enable = true },
       indent = { enable = true }
     })
